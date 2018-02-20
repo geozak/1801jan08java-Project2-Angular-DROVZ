d7489e4209a6fc6826882bf5b0a9743b8e4e58a9
Zakary Stone <zakarystone@gmail.com>
2018-02-20 02:10:27 -0500

    zero formatting trainer login with single mock trainer

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { AuthGuard } from './guards/auth-guard.service';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'welcome', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
