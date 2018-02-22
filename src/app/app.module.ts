2946f83c4992bd7b2a82b5b52dfa630341dcd664
vladigleba <hi@vladigleba.com>
2018-02-22 10:01:40 -0500

    view users profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { HttpClientModule } from '@angular/common/http';
 import { AuthGuard } from './guards/auth-guard.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -10,23 +9,29 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { AuthService } from './services/auth.service';
-
+import { PostService } from './services/post.service';
+import { PostComponent } from './components/post/post.component';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,
-    HttpClientModule
+    HttpModule,
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [
     AuthService,
+    PostService,
     AuthGuard
   ],
   bootstrap: [AppComponent]
---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Post } from '../../models/post';
+import { PostService } from '../../services/post.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+  posts: Post[];
+
+  constructor(private postService: PostService) { }
+
+  ngOnInit() {
+    this.getPosts();
+  }
+
+  getPosts(): void {
+    this.posts = this.postService.getPosts();
+  }
+
+  createPost(post: string): void {
+    post = post.trim();
+    if (!post) { return; }
+
+    const postObj: Post = {
+      post_id: this.posts.length + 1,
+      post_desc: post,
+      trainer_id: JSON.parse(localStorage.getItem('currentTrainer')).id,
+      post_timestamp: new Date().toString(),
+      likers_id: []
+    };
+
+    this.posts.push(postObj);
+  }
+
+  likePost(post: Post): void {
+    const liker_id = JSON.parse(localStorage.getItem('currentTrainer')).id;
+
+    if (post.likers_id.includes(liker_id) {
+      const index = post.likers_id.indexOf(liker_id);
+      post.likers_id.splice(index, 1);
+    } else {
+      post.likers_id.push(liker_id);
+    }
+  }
+}
