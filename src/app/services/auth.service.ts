e62eddf9f9a564a297fb4428e3d6876e43e04070
vladigleba <hi@vladigleba.com>
2018-02-27 09:43:41 -0500

    registration and password reset

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,33 +13,34 @@ export class AuthService {
   login(email: string, password: string) {
     console.log('logging in');
 
-    // let demo: Trainer | null;
-    // if (email === 'zak' && password === '1234') {
-    //   demo = {
-    //     id: 1,
-    //     email: email,
-    //     url: 'myUsername',
-    //     firstName: 'myFirst',
-    //     lastName: 'myLast',
-    //     profilePictureUrl: 'someurl'
-    //   };
-    // } else {
-    //   demo = null;
-    // }
-    // return of<any>(demo)
-    //   .map(trainer => {
-    //     console.log('mapping');
-    //     console.log(trainer);
-    //     // login successful if there's a jwt token in the response
-    //     if (trainer) {// && trainer.token) {
-    //       console.log('saving trainer');
-    //       // store trainer details and jwt token in local storage to keep trainer logged in between page refreshes
-    //       localStorage.setItem('currentTrainer', JSON.stringify(trainer));
-    //     }
+    /*
+    let demo: Trainer | null;
+    if (email === 'zak' && password === '1234') {
+      demo = {
+        id: 1,
+        email: email,
+        url: 'myUsername',
+        firstName: 'myFirst',
+        lastName: 'myLast',
+        profilePictureUrl: 'someurl'
+      };
+    } else {
+      demo = null;
+    }
+    return of<any>(demo)
+      .map(trainer => {
+        console.log('mapping');
+        console.log(trainer);
+        // login successful if there's a jwt token in the response
+        if (trainer) {// && trainer.token) {
+          console.log('saving trainer');
+          // store trainer details and jwt token in local storage to keep trainer logged in between page refreshes
+          localStorage.setItem('currentTrainer', JSON.stringify(trainer));
+        }
 
-    //     return trainer;
-    //   });
-
+        return trainer;
+      });
+    */
 
     const formdata: FormData = new FormData();
     formdata.append('email', email);
