e82793959cf4a943bb13764973405c8597199185
Zakary Stone <zakarystone@gmail.com>
2018-02-27 20:30:23 -0500

    added register to auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,20 +4,21 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import { of } from 'rxjs/observable/of';
 import { Trainer } from '../models/trainer';
+import { domain } from '../globals';
 
 @Injectable()
 export class AuthService {
 
   constructor(private http: HttpClient) { }
 
-  login(email: string, password: string) {
+  login(email: string, password: string): Observable<Trainer | null > {
     console.log('logging in');
 
     const formdata: FormData = new FormData();
     formdata.append('email', email);
     formdata.append('password', password);
 
-    return this.http.post<Trainer | null>('http://localhost:8090/login', formdata,
+    return this.http.post<Trainer | null>(domain + '/login', formdata,
       { headers: new HttpHeaders({ 'Access-Control-Allow-Origin': '*'})})
         .map(trainer => {
           console.log('mapping:');
@@ -29,6 +30,20 @@ export class AuthService {
 
           return trainer;
         });
+  }
+
+  // string will be one of { "success", "inputs", "url", "email", "other" }
+  // all but "success" are failures
+  register(firstName: string, lastName: string, email: string, password: string): Observable<string> {
+    console.log('registering');
+
+    const formdata: FormData = new FormData();
+    formdata.append('firstName', firstName);
+    formdata.append('lastName', lastName);
+    formdata.append('email', email);
+    formdata.append('password', password);
+
+    return this.http.post<string>(domain + '/register', formdata, { headers: new HttpHeaders({ 'Access-Control-Allow-Origin': '*' }) });
   }
 
   logout(): void {
