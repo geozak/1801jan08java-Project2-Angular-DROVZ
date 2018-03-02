4e924b20a23ced69d2158facc4f5757e2e9ebba7
momentumrick <impetrified@hotmail.com>
2018-03-01 22:14:22 -0600

    added upload picture to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Trainer } from './../../models/trainer';
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpResponse, HttpEventType } from '@angular/common/http';
+import { UploadFileService } from '../../services/upload-file.service';
+
 
 @Component({
   selector: 'app-home',
@@ -9,8 +12,13 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
   currentTrainer: Trainer;
   isEdit: Boolean = false;
+  isHovered: Boolean = false;
 
-  constructor() {
+  selectedFiles: FileList
+  currentFileUpload: File
+  progress: { percentage: number } = { percentage: 0 }
+
+  constructor(private uploadService: UploadFileService) {
     this.currentTrainer = JSON.parse(localStorage.getItem('currentTrainer'));
     console.log(this.currentTrainer);
   }
@@ -35,5 +43,36 @@ export class HomeComponent implements OnInit {
       this.currentTrainer.email = email;
     }
   }
+
+  selectFile(event) {
+    var fileName = (<HTMLInputElement>(document.getElementById("fileName"))).value;
+    var idxDot = fileName.lastIndexOf(".") + 1;
+    var extFile = fileName.substr(idxDot, fileName.length).toLowerCase();
+    if (extFile=="jpg" || extFile=="jpeg" || extFile=="png" || extFile=="gif"){
+      this.selectedFiles = event.target.files;
+      this.upload();
+    }else{
+        alert("Only jpg/jpeg and png files are allowed!");
+    }
+}
+upload() {
+  this.progress.percentage = 0;
+
+  this.currentFileUpload = this.selectedFiles.item(0)
+  this.uploadService.updateTrainerPhoto(this.currentFileUpload, this.currentTrainer.id).subscribe(event => {
+    if (event.type === HttpEventType.UploadProgress) {
+      this.progress.percentage = Math.round(100 * event.loaded / event.total);
+    } else if (event instanceof HttpResponse) {
+      console.log(<string>event.body);
+      this.currentTrainer.profilePictureUrl = <string>event.body;
+      localStorage.setItem('currentTrainer', JSON.stringify(this.currentTrainer));
+      console.log('File is completely uploaded!');
+    }
+  }
+  , err => alert(err)
+)
+
+  this.selectedFiles = undefined
+}
 
 }
--- src/app/services/upload-file.service.ts
+++ src/app/services/upload-file.service.ts
@@ -20,7 +20,20 @@ export class UploadFileService {
  
     return this.http.request(req);
   }
+  updateTrainerPhoto(file: File, trainerId: number): Observable<HttpEvent<{}>> {
+    let formdata: FormData = new FormData();
  
+    formdata.append('file', file);
+    formdata.append('trainerId', `${trainerId}`);
+ 
+    const req = new HttpRequest('POST', 'http://localhost:8090/postPhoto.app', formdata, {
+      headers: new HttpHeaders({ 'Access-Control-Allow-Origin': '*'}),
+      reportProgress: true,
+      responseType: 'text'
+    });
+ 
+    return this.http.request(req);
+  }
   getFiles(): Observable<Object> {
     return this.http.get('/getallfiles')
   }
