a7c9ac4ab8266bbf0bdbc6ae0b90c0ed7844af5e
Oscar5273 <oscarvasquez17@gmail.com>
2018-03-04 14:12:07 -0500

    PostUpdate

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,3 +1,3 @@
 // export const domain = 'http://192.168.0.9:8090'; // f
-// export const domain = 'http://localhost:8090'; // all spring boot / java application
-export const domain = 'http://localhost:9001/Project2-DROVZ'; // zak tomcat
+ export const domain = 'http://localhost:8090'; // all spring boot / java application
+// export const domain = 'http://localhost:9001/Project2-DROVZ'; // zak tomcat
