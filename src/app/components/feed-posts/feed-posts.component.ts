4249bb18a0c0f4f921288d20dbe9ac0861214fbc
Devin-Dellamano <devin_dellamano@yahoo.com>
2018-03-04 14:15:35 -0500

    Modified the feed page a little bit to show the person's name that created the post.

--- src/app/components/feed-posts/feed-posts.component.ts
+++ src/app/components/feed-posts/feed-posts.component.ts
@@ -1,3 +1,4 @@
+import { PostService } from './../../services/post.service';
 import { Post } from './../../models/post';
 import { Trainer } from './../../models/trainer';
 import { Component, OnInit } from '@angular/core';
@@ -12,9 +13,60 @@ export class FeedPostsComponent implements OnInit {
   trainers: Trainer[] = [];
   posts: Post[] = [];
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
+    this.getPosts();
+  }
+
+  getPosts(): void {
+
+    this.postService.getPosts().subscribe(
+      allPosts => {
+        this.posts = allPosts;
+        console.log(this.posts);
+      }
+    );
+    // this.postService.getAllbyTrainer(JSON.parse(localStorage.getItem('currentTrainer')).id).subscribe(r=>this.posts = r);
+
+  }
+
+  likePost(post: Post): void {
+    const liker = JSON.parse(localStorage.getItem('currentTrainer'));
+
+    if (post.likedBy.includes(liker)) {
+      // unlike
+      post.likedBy.splice(post.likedBy.indexOf(liker), 1);
+
+      // persist to db
+      const response = this.postService.unlike(post.id, liker.id);
+
+      // subscribe
+      response.subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    } else {
+      // like
+      post.likedBy.push(liker);
+
+      // persist to db
+      const response = this.postService.like(post.id, liker.id);
+
+      // subscribe
+      response.subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
   }
 
 }
